ef6c05cc6781604f1c69c111ac51d5e6a5cbf8da
Hezio Fernandes <hezinho@Hezios-MacBook-Pro.local>
2021-02-13 15:44:51 +0000

    Tests done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data)
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: 'POC integration of TestCafe, CucumberJS and Allure Report',
+      url: 'https://github.com/heziofernandes/cucumber-testcafe-allure',
+      techs: ['NodeJS', 'TestCafe', 'Cucumber']
+    })
+
+    setRepositories([...repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+
+  const newRepository =  repositories.filter(
+    repository => repository.id !== id
+  )
+
+    setRepositories(newRepository)
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
